eac6723ed70f9b53ae3256cbfc52fb6de719178f
matthewliu <matt@originprotocol.com>
2025-03-12 20:39:48 -0700

    Setup openai-agents

--- config/constants.py
+++ config/constants.py
@@ -6,10 +6,4 @@ load_dotenv()
 HOST = os.getenv('HOST')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-DEV_EMAIL = os.getenv('DEV_EMAIL')
-
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_ADMIN_CHAT_ID = os.getenv('TELEGRAM_ADMIN_CHAT_ID')
-
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
